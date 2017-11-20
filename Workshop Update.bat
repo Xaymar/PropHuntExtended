3607102698eb9caf7727b48acc4145ef9c8c9c0c
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2017-11-20 05:03:21 +0100

    media: Use new and improved workshop logo

--- Workshop Update.bat
+++ Workshop Update.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 call env.win.bat
-"%GARRYSMODPATH%\bin\gmpublish.exe" update -id 468149739 -icon "Media\Logo.jpg" -addon "Pack.gma" -changes ""
+"%GARRYSMODPATH%\bin\gmpublish.exe" update -id 468149739 -icon "media\gamemode_workshop.jpg" -addon "Pack.gma" -changes ""
 PAUSE
